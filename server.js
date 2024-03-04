654d1bfd97bb971878aa82fc10a9db5361e22a94
John Nelson <jnelson6689@gmail.com>
2024-03-04 18:43:02 -0500

    added files, made changes to json file.

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require("express");
 const port = process.env.PORT || 8000;
 const app = express();
 
-const Message = require('./Message.js')
+const Message = require('./model/Message.js')
 
 
 
 app.listen(port, () => {
-  console.log('listening on port: ' + port) 
+  console.log('listening on port: ' + port)
 })
