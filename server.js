f3b20f18b7b127355e086cd7261984704d39341f
jagome05 <jagome05@gmail.com>
2024-03-07 21:44:54 -0500

    Added middleware fxn for admin privelege and updated users model for isAdmin icebox

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const { dbConnect } = require('./db')
 //connects to controllers
 const messages = require('./controllers/message.js')
 // const users = require('./controllers/users.js')
+const admin = require('./middleware/isAdmin')
 
 // * lets us read json
 app.use(express.json())
@@ -19,6 +20,8 @@ app.use(express.json())
 // connects to endpoints
 app.use('/api', messages)
 // app.use('/users', users)
+//* use this to test admin privelge ---> app.use('/admin', admin)
+
 
 app.listen(PORT, () => {
   dbConnect()
